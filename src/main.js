5aec03e6e49c7161927541ce485120570beb9440
Lloyd <lloyd.gerry@gmail.com>
2019-11-23 14:25:55 -0800

    adding routes and app update to include links

--- src/main.js
+++ src/main.js
@@ -2,13 +2,15 @@
 
 
 import Vue from 'vue';
-import VueRouter from 'vue-router';
 import App from './App.vue'
+import router from './router'
+import store from './store'
 
-Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
