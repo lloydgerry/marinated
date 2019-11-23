fe9a5f03ccf8bd71f51f04237fb765714a4c4875
Lloyd <lloyd.gerry@gmail.com>
2019-11-22 18:22:47 -0800

    adding tags seeds

---
+++ src/db/seeds/tags_seeds.sql
@@ -0,0 +1,24 @@
+INSERT INTO tags (name)
+VALUES
+("meat"),
+("meatless"),
+("no-meat"),
+("dinner"),
+("lunch"),
+("breakfast"),
+("milk/cream"),
+("apples"),
+("oranges"),
+("ginger"),
+("cookies"),
+("cheese"),
+("polenta"),
+("squash"),
+("sausage"),
+("pasta"),
+("cashews"),
+("beans"),
+("oats"),
+("steelcutoats"),
+("eggs")
+("pancakes")
