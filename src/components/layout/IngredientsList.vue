86e612555ca8840a0a9c50fa7d2d052d98585959
Lloyd <lloyd.gerry@gmail.com>
2019-11-25 13:06:33 -0800

    layout wireframe working for newrecipe

---
+++ src/components/layout/IngredientsList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    {{ingredientslist}}
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    // IngredientsList,
+  },
+  // data function avails data to the template
+  data() {
+    return {
+      ingredientslist: [
+        "sugar",
+        "flour"
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/views/NewRecipe.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+      <div>
+        <h2>Enter a URL</h2>
+        <input type="text" v-model="scrapeurl" />
+        <input type="submit" value="Submit URL Scape" v-on:click="submit" />
+      </div>
+      <div class="new-recipe-title">
+        <h2>Title</h2>
+        <input type="text" v-model="title" />
+      </div>
+      <div>
+        <h2>Image Link</h2>
+        <input type="text" v-model="imagelink" />
+      </div>
+       <div>
+        <h2>Author</h2>
+        <input type="text" v-model="author" />
+      </div>
+       <div>
+        <h2>Source URL</h2>
+        <input type="text" v-model="sourceurl" />
+      </div>
+       <div>
+        <h2>Prep Time</h2>
+        <input type="text" v-model="preptime" />
+      </div>
+       <div>
+        <h2>Servings</h2>
+        <input type="text" v-model="servings" />
+      </div>
+      <div>
+        <h2>Ingredients</h2>
+        <input type="text" v-model="servings" />
+        <IngredientsList/>
+      </div>
+      <div>
+        <h2>Preparation</h2>
+        <input type="text" v-model="preparation" />
+      </div>
+      <div class="btn">
+        <input type="submit" value="Create New Recipe" v-on:click="submit" />
+      </div>
+    </div>
+</template>
+
+<script>
+import IngredientsList from '../components/layout/IngredientsList'
+
+
+export default {
+  components: {
+   IngredientsList,
+  },
+  data: () => ({
+      scrapeurl: '',
+      title: 'your recipe',
+      imagelink: 'https://',
+      author: '',
+      sourceurl: '',
+      preptime: '',
+      servings: '',
+      ingredients: '',
+      preparation: '',
+  }),
+  methods: {
+    submit: function() {
+      console.log(this.scrapeurl, this.title, this.imagelink);
+
+      this.scrapeurl = '';
+      this.title = '';
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
