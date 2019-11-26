6e66184a5b55afb26c508c5c2721624311352ba3
Lloyd <lloyd.gerry@gmail.com>
2019-11-25 19:41:24 -0800

    recipe card is now rendering cards and sortable

--- src/components/layout/RecipeCardsCarousel.vue
+++ src/components/layout/RecipeCardsCarousel.vue
@@ -1,39 +1,45 @@
 <template>
     <div > 
         <draggable v-model="recipeCarouselArray">
-            <transition-group class="recipe-carousel">
-                <div v-for="recipe in recipeCarouselArray" :key="recipe.id" class="recipe-carousel-item">
-                    {{recipe.title}}
+            <transition-group class="recipe-carousel centered">
+                <div v-for="recipe in recipeCarouselArray" :key="recipe.id" class="recipe-carousel-item recipe-card">
+                  <RecipeCard v-bind:recipe="recipe" class="recipe-carousel-item recipe-card" />
                 </div>
             </transition-group>
         </draggable>
+
     </div>
 </template>
 
 <script>
 import draggable from 'vuedraggable'
+import RecipeCard from './RecipeCard'
 
   export default {
         components: {
             draggable,
+            RecipeCard,
         },
         data () {
             return {
                 recipeCarouselArray: [
                     {
-                        id: 1,
-                        title: "card 1",
-                        summary: "stuffstuffstuff"
+                      id: 1,
+                      title: "card 1",
+                      summary: "stuffstuffstuff",
+                      img_url: "https://i.ndtvimg.com/i/2015-11/avocado_625x350_71446534495.jpg"
                     },
                     {
-                        id: 2,
-                        title: "is this a title?",
-                        summary: "oh man this is a summary"
+                      id: 2,
+                      title: "is this a title?",
+                      summary: "oh man this is a summary",
+                      img_url: "https://i.ndtvimg.com/i/2015-11/avocado_625x350_71446534495.jpg"
                     },
                     {
-                        id: 3,
-                        title: "this is another item",
-                        summary: "another summary"
+                      id: 3,
+                      title: "this is another item",
+                      summary: "another summary",
+                      img_url: "https://i.ndtvimg.com/i/2015-11/avocado_625x350_71446534495.jpg"
                     }
                 ],
             }
