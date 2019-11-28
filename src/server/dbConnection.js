ce37efd050b91971823f2d791110ba402d1e8239
Lloyd <lloyd.gerry@gmail.com>
2019-11-27 18:09:42 -0800

    trying to fix connection bugs in dbconnection file as heroku

--- src/server/dbConnection.js
+++ src/server/dbConnection.js
@@ -4,9 +4,6 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
   connectionString: process.env.DATABASE_URL,
   ssl: true,
 });
