9d554a140aa6e5616c59fce8a371059ea30b33d3
Lloyd <lloyd.gerry@gmail.com>
2019-11-27 18:26:18 -0800

    committing dist files

---
+++ dist/js/about.04f398a5.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,a,e){"use strict";e.r(a);var n=function(){var t=this,a=t.$createElement,e=t._self._c||a;return e("div",[e("NavBar"),t._m(0)],1)},s=[function(){var t=this,a=t.$createElement,e=t._self._c||a;return e("div",{staticClass:"about"},[e("h1",[t._v("This is an about page")])])}],u=e("f464"),o={name:"About",components:{NavBar:u["a"]}},r=o,c=e("2877"),i=Object(c["a"])(r,n,s,!1,null,null,null);a["default"]=i.exports}}]);
+//# sourceMappingURL=about.04f398a5.js.map
