fa49dbb3a456b057270a0da2feee63b8e6374c9a
Lloyd <lloyd.gerry@gmail.com>
2019-11-29 14:46:06 -0800

    state now in store for recipes, with home calling recipes on created

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+const axios = require('axios')
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    recipes: []
   },
   mutations: {
+    setRecipes(state, newRecipes) {
+      state.recipes = newRecipes;
+    }
   },
   actions: {
+    fetchData({commit}) {
+      axios.get('/api/recipes')
+        .then(res => {
+          commit('setRecipes',  res.data) ;
+          console.log(" returning the recipe data in store: ", res.data);
+          console.log("store state recipes: ", this.state.recipes)
+        })
+        .catch(error => console.log("error from fetch data in store", error));
+    }
   },
   modules: {
-  }
+  },
+  //  
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,8 +15,9 @@ import NavBar from "../components/layout/NavBar.vue"
 import DivSpace from "../components/layout/DivSpace.vue"
 import PlanDrawer from "../components/PlanDrawer"
 import RecipeCardsCarousel from "../components/layout/RecipeCardsCarousel"
-const axios = require('axios')
+// const axios = require('axios')
 
+import { mapActions } from 'vuex';
 
 export default {
   name: 'home',
@@ -39,20 +40,21 @@ export default {
     this.fetchData()
   },
   methods: {
+    ...mapActions(['fetchData']),
     mealCardClose() {
       this.mealCardIsOpen = false
     },
     mealCardOpen() {
       this.mealCardIsOpen = true
     },
-    fetchData() {
-      axios.get('/api/recipes')
-        .then(res => {
-          this.RecipeCards = res.data;
-          console.log("vue returning the recipe data: ", res.data);
-        })
-        .catch(error => console.log("error from fetch data", error));
-    }
+    // fetchData() {
+    //   axios.get('/api/recipes')
+    //     .then(res => {
+    //       this.RecipeCards = res.data;
+    //       console.log("vue returning the recipe data: ", res.data);
+    //     })
+    //     .catch(error => console.log("error from fetch data", error));
+    // }
   }
 }
 </script>
