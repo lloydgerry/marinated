2848e5e8d5beca6b68b28a925456e5c84176358d
Graeme <graeme.nickerson@gmail.com>
2019-11-28 17:28:53 -0800

    Imports new dragging dependency and starts work on mealplan page.

--- src/views/MealPlan.vue
+++ src/views/MealPlan.vue
@@ -2,22 +2,84 @@
   <div>
   <NavBar/>
   <DivSpace/>
-  <div class="about">
-    <h1>This is the Meal Plan page</h1>
-  </div>
+    <div class="simple-page">
+        <div class="menu">
+          <Container group-name="1" :get-child-payload="getChildPayload2" @drop="onDrop('items2', $event)">
+            <Draggable v-for="item in items2" :key="item.id" :orientation="horizontal">
+              <div class="draggable-item-horizontal">
+                {{item.data}}
+              </div>
+            </Draggable>
+            
+          </Container>
+        </div>
+        <div class="bottom-bar">
+        <div class="delete-area">
+          <Container group-name="1" :get-child-payload="getChildPayload2" @drop="onDelete('items2', $event)" >
+            <Draggable v-for="item in deleteThis" :key="item.id" :orientation="horizontal">
+              <div class="draggable-item-horizontal">
+                {{item.data}}
+              </div>
+            </Draggable>
+          </Container>
+        </div>
+        <div >
+          <Container behaviour="copy" group-name="1" :get-child-payload="getChildPayload1" class="card-carousel">
+            <Draggable v-for="item in items1" :key="item.id">
+              <div class="draggable-item-horizontal">
+                {{item.data}}
+              </div>
+            </Draggable>
+          </Container>
+        </div>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
-import NavBar from "../components/layout/NavBar.vue"
-import DivSpace from "../components/layout/DivSpace.vue"
-
+  import NavBar from "../components/layout/NavBar.vue";
+  import DivSpace from "../components/layout/DivSpace.vue";
+  import { Container, Draggable } from "vue-smooth-dnd";
+  import { applyDrag, generateItems } from "../utils/helpers";
 
 export default {
   name: 'MealPlan',
-  components: {
+  components: { 
     NavBar,
-    DivSpace
+    DivSpace,
+    Container, 
+    Draggable 
+  },
+  data() {
+    return {
+      items1: generateItems(30, i => ({
+        id: '1' + i,
+        data: `Source Draggable - ${i}`
+      })),
+      items2: generateItems(21, i => ({
+        id: '2' + i,
+        data: `Draggable 2 - ${i}`
+      })),
+      deleteThis: []
+    };
+  },
+  methods: {  
+    onDrop (collection, dropResult) {
+      this[collection] = applyDrag(this[collection], dropResult);
+    },
+    onDelete(collection, dropResult) {
+      this.deleteThis = applyDrag(this[collection], dropResult);
+      this.deleteThis = [];
+    },
+    getChildPayload1 (index) {
+      return this.items1[index]
+    },
+    getChildPayload2 (index) {
+      return this.items2[index]
+    }
   }
-}
+};
 </script>
+
+<style scoped src="../styles/css/MealPlan.css" lang="css"/>
