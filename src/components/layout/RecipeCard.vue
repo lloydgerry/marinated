4826fcd88e9d5a7bdc807c643f6bb8dbbe02fb26
Graeme <graeme.nickerson@gmail.com>
2019-11-29 18:55:13 -0800

    Updates styling for home page recipe cards.

--- src/components/layout/RecipeCard.vue
+++ src/components/layout/RecipeCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card-container">
-    <router-link :to="{ name: 'recipe', params: { id: recipe.id } }" class="card u-clearfix">
+    <router-link :to="{ name: 'recipe', params: { id: recipe.id } }" class="card-link">
       <img v-bind:src=recipe.image_url  alt="" class="card-media" />
       <!-- <span class="card-number card-circle subtle">{{recipeCard.id}}</span> -->
       <h2 class="card-title">{{recipe.title}} </h2>
