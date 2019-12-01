d74a94b7543c558172b47baafa79583a2c73702c
Lloyd <lloyd.gerry@gmail.com>
2019-11-30 19:19:29 -0800

    new-recipe page styling and front end error handling aded

--- src/components/layout/IngredientsList.vue
+++ src/components/layout/IngredientsList.vue
@@ -1,14 +1,9 @@
 <template>
   <div>
     <ul class='ingredients-list'>
-      <li class='ingredients-list-item' v-for="ingredient in ingredients" v-bind:key="ingredient.index" >
+      <li class='ingredients-list-item' v-for="ingredient in ingredients" v-bind:key="ingredient" >
        - {{ingredient}}
       </li>
-      <form v-if="seen" @submit="addIngredient">
-      <input type="text" v-model="name"  v-on:keyup.enter="submit" placeholder="Type your ingredient here" > 
-      <input type="submit" value="+">
-      </form>
-      <p><button class="btn" v-on:click="seen = !seen"> Add Ingredient </button></p>
     </ul>
   </div>
 </template>
@@ -21,18 +16,9 @@ export default {
   data() {
     return {
       ingredientslist: [],
-      seen: false,
-      name: ''
     };
   },
   methods: {
-    addIngredient(e) {
-      e.preventDefault();
-      console.log("name from addIngredient in InList", this.name)
-      this.$emit('new-ingredient', this.name );
-      this.name = "";
-      this.seen = false;
-    }
   },
 };
 </script>
