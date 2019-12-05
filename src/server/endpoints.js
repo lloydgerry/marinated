6122e3290ca7ba0779e65a9cd63ef9e092008a90
Lloyd <lloyd.gerry@gmail.com>
2019-12-05 11:26:18 -0800

    adding search terms to endpoint with temp logging

--- src/server/endpoints.js
+++ src/server/endpoints.js
@@ -9,27 +9,52 @@ module.exports = function(router) {
 
     if (error) {console.log(error)}
     
-    console.log("request", request)
-    console.log("response", response)
+    console.log("request search", request.body.search)
+    const search_query = request.body.search
+    console.log("query", search_query )
 
+    if (search_query !== undefined ) {
 
-    const dbQuery = `
+      const dbQuery = `
       SELECT * FROM recipes 
+      WHERE search_array @@ plainto_tsquery($1)
       ORDER BY random()
-      limit 2;
+      limit 1
       `;
+      const dbParams = [search_query]
 
-    return client.query(dbQuery)
-      .then(recipes => {
-        release()
-        let url = recipes.rows[0].id
-        const thingy = `http://marinated.herokuapp.com/#/recipe/${url}`
-        response.json({thingy}) 
+      return client.query(dbQuery, dbParams)
+        .then(recipes => {
+          release()
+          let url = recipes.rows[0].id
+          const thingy = `http://marinated.herokuapp.com/#/recipe/${url}`
+          response.json({thingy}) 
 
-      })
-      .catch(error => {
-        console.error('', error);
-      })
+        })
+        .catch(error => {
+          console.error('', error);
+        })
+
+    } else {
+
+      const dbQuery = `
+        SELECT * FROM recipes 
+        ORDER BY random()
+        limit 1;
+        `;
+
+      return client.query(dbQuery)
+        .then(recipes => {
+          release()
+          let url = recipes.rows[0].id
+          const thingy = `http://marinated.herokuapp.com/#/recipe/${url}`
+          response.json({thingy}) 
+
+        })
+        .catch(error => {
+          console.error('', error);
+        })
+        }
     })
   })
 
